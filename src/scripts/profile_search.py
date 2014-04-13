e3418656bb86d5700f4a34e60e4b8b29d787801a
Karthik Venugopal <kavi.develog@gmail.com>
2014-04-13 18:13:38 -0500

    Added location to experience data

--- src/scripts/profile_search.py
+++ src/scripts/profile_search.py
@@ -5,21 +5,13 @@ from scrapy.crawler import Crawler
 from scrapy.settings import Settings
 from scrapy.xlib.pydispatch import dispatcher
 from scrapy import log, signals
-from scrapy_wrapper import LinkedInSpider
-from scrapy_wrapper import AuthLinkedInSpider
+from scrapy_wrapper import *
 from twisted.internet import reactor
 
 
 def stop_reactor():
     reactor.stop()
 
-
-people_map = ['http://in.linkedin.com/pub/nitish-kumar/45/58b/29', 'http://www.linkedin.com/in/viditochani',
-              'http://www.linkedin.com/pub/denis-bellavance/5/b15/63a', 'http://www.linkedin.com/in/seansharma',
-              'http://www.linkedin.com/pub/madhwaraj-g-k/10/98/64', 'http://www.linkedin.com/in/kgillett',
-              'http://www.linkedin.com/in/semihenergin', 'http://www.linkedin.com/in/sharathchandrapilli',
-              'http://www.linkedin.com/pub/nitish-kackar/a/b07/635', 'http://www.linkedin.com/in/rkots',
-              'http://www.linkedin.com/pub/amit-nain/19/519/47a', 'http://www.linkedin.com/in/rachitdhall']
 
 dispatcher.connect(stop_reactor, signal=signals.spider_closed)
 
@@ -30,7 +22,7 @@ crawler.configure()
 crawler.crawl(linkedin_spider)
 crawler.start()
 
-# log.start(loglevel=log.DEBUG, crawler=crawler, logstdout=True)
+log.start(loglevel=log.DEBUG, crawler=crawler, logstdout=True)
 
 reactor.run()
 
