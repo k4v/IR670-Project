a9399e931bec5f276b6b41af6b355525f9fbb4d1
Karthik Venugopal <kavi.develog@gmail.com>
2014-04-16 01:14:56 -0500

    Checking similarity for all companies

--- src/scripts/scrapy_reader.py
+++ src/scripts/scrapy_reader.py
@@ -3,5 +3,20 @@ __author__ = 'karthik'
 import cPickle
 
 dump_file = open('all_companies.json', 'r')
-item = cPickle.load(dump_file)
-print item
+profile_dump = cPickle.load(dump_file)
+
+
+def get_company_dump(company_name):
+    profile_list = []
+    company_name = unicode(company_name.strip().lower())
+
+    for profile in profile_dump:
+        add_profile = False
+        for exp_item in profile_dump[profile]['experience']:
+            if exp_item.company.strip().lower().find(company_name) >= 0 or \
+                    company_name.find(exp_item.company.strip().lower()) >= 0:
+                add_profile = True
+        if add_profile:
+            profile_list.append(profile_dump[profile])
+
+    return profile_list
