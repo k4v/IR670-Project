8ba03706c56e0845fae06b4576e63aae7879178e
Nikhil <nikhils88@gmail.com>
2014-04-20 23:37:55 -0500

    k-means cluster based on PyCluster
    k-means cluster based on PyCluster

--- src/scripts/skill_cluster.py
+++ src/scripts/skill_cluster.py
@@ -2,30 +2,37 @@ __author__ = 'Nikhil'
 
 import scrapy_reader
 import get_keywords
+import Pycluster
 import types
 from scipy.cluster.vq import vq, kmeans2, whiten
 from numpy import *
 
+k = 10  # k-means no: of clusters
 tokenList = []
 vectorList = []
 profileVector = []
 profileList = scrapy_reader.profile_dump
 
+i=0
 for profile in profileList.itervalues():
+    i += 1
     tokenList.extend([x.lower() for x in profile['skills']])
     if len(profile['experience']) != 0:
         exp = profile['experience'][0]
     if type(exp.desc) is not types.NoneType:
         descTokens = get_keywords.get_keywords(exp.desc)
         tokenList.extend(descTokens)
+    if i == 3:
+        break
 tokenS = set(tokenList)
 tokenList = list(tokenS)
 
 '''
 Forming token vectors and profiling of users
 '''
-
+i = 0
 for profile in profileList.itervalues():
+    i += 1
     tokens = []
     tokens.extend([x.lower() for x in profile['skills']])
     if len(profile['experience']) != 0:
@@ -33,13 +40,20 @@ for profile in profileList.itervalues():
     if type(exp.desc) is not types.NoneType:
         descTokens = get_keywords.get_keywords(exp.desc)
         tokens.extend(descTokens)
+    tempTokens = set(tokens)
+    tokens = list(tempTokens)
+    #print tokens
     profileVector.append([exp.company.lower(), exp.postitle.lower()])
-    tokenV = [0] * len(tokenList)
+    tokenV = [0.0] * len(tokenList)
     j = 0
     for t in tokenList:
         if t in tokens:
-            tokenV[j] = 1
+            tokenV[j] = 1.0
+        j += 1
     vectorList.append(tokenV)
+    #print tokenV
+    if i == 3:
+        break
 
 print vectorList
 print profileVector
@@ -48,7 +62,11 @@ print len(vectorList)
 
 features = array(vectorList)
 #whitened = whiten(features)
-#centroids, index = kmeans2(data=whitened, k=10)
-#print centroids
+
+labels, error, nfound = Pycluster.kcluster(features, 2)
+centroids = vstack([features[labels == i].mean(0) for i in range(labels.max() + 1)])
+
+print labels
+print centroids
 
 
